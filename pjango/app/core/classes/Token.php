249437cb66d2a61ed8c36eb12302d564f41f34b1
Afari Samuel Adusei <thinkcetechnologies@gmail.com>
2024-10-05 10:46:34 +0000

    payment options

--- pjango/app/core/classes/Token.php
+++ pjango/app/core/classes/Token.php
@@ -3,8 +3,7 @@ class Token{
     public static function generate(){
         return Session::put(Config::get('session/token_name'), md5(uniqid()));
     }
-    public static function check($token): bool
-    {
+    public static function check($token): bool{
         $tokenName = Config::get('session/token_name');
         if(Session::exists($tokenName) && $token === Session::get($tokenName)){
             Session::delete($tokenName);
